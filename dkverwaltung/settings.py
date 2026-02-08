11f7a53f78f524fe1a3ca388365df8f6f40abebc
Lu <lu@kalkbrenner.in>
2026-02-08 22:43:35 +0100

    set static roor dir to fix thanks mails

--- dkverwaltung/settings.py
+++ dkverwaltung/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # needed for pm collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'dkapp'), 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
